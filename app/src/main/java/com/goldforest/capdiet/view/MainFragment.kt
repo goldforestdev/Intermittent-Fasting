b812c620d9feccd07dbabd155cb614fd7a1f65b9
hyeongju-kim <kinghj.kim@gmail.com>
2019-05-16 02:20:36 +0900

    add base fragment

--- app/src/main/java/com/goldforest/capdiet/view/MainFragment.kt
+++ app/src/main/java/com/goldforest/capdiet/view/MainFragment.kt
@@ -2,36 +2,34 @@ package com.goldforest.capdiet.view
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.goldforest.capdiet.R
+import com.goldforest.capdiet.base.BaseFragment
+import com.goldforest.capdiet.databinding.FragmentMainBinding
+import com.goldforest.capdiet.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
+class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_main
+    override val viewModel: MainViewModel by viewModel()
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class MainFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    override fun initStartView() {
+    }
+
+    override fun initDataBinding() {
+    }
+
+    override fun initAfterBinding() {
+
     }
 
 }
