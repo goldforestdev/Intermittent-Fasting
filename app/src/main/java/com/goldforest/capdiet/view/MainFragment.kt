f233b64d23797c9014f581abb8f983ab208cb841
hyeongju-kim <kinghj.kim@gmail.com>
2019-05-17 00:51:25 +0900

    PlanFragment 연결

--- app/src/main/java/com/goldforest/capdiet/view/MainFragment.kt
+++ app/src/main/java/com/goldforest/capdiet/view/MainFragment.kt
@@ -4,10 +4,12 @@ package com.goldforest.capdiet.view
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.goldforest.capdiet.R
 import com.goldforest.capdiet.base.BaseFragment
 import com.goldforest.capdiet.databinding.FragmentMainBinding
 import com.goldforest.capdiet.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.fragment_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
@@ -19,16 +21,9 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.show()
+
+        btPlanViewTest.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_planFragment)
+        }
     }
-
-    override fun initStartView() {
-    }
-
-    override fun initDataBinding() {
-    }
-
-    override fun initAfterBinding() {
-
-    }
-
 }
--- app/src/main/java/com/goldforest/capdiet/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/goldforest/capdiet/viewmodel/MainViewModel.kt
@@ -5,4 +5,5 @@ import com.goldforest.capdiet.base.BaseViewModel
 
 class MainViewModel : BaseViewModel() {
 
+
 }
