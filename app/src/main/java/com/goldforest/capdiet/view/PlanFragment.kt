50b18ed19a41f7a0698013844983081d5581f921
hyeongju-kim <kinghj.kim@gmail.com>
2019-05-20 00:27:42 +0900

    Plan Type에 따른 View 설정 화면 변경

--- app/src/main/java/com/goldforest/capdiet/view/PlanFragment.kt
+++ app/src/main/java/com/goldforest/capdiet/view/PlanFragment.kt
@@ -3,17 +3,15 @@ package com.goldforest.capdiet.view
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.goldforest.capdiet.R
 import com.goldforest.capdiet.base.BaseFragment
 import com.goldforest.capdiet.databinding.FragmentPlanBinding
 import com.goldforest.capdiet.viewmodel.PlanType
 import com.goldforest.capdiet.viewmodel.PlanViewModel
+import kotlinx.android.synthetic.main.fragment_plan.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -21,12 +19,18 @@ class PlanFragment : BaseFragment<FragmentPlanBinding, PlanViewModel>() {
 
     override val layoutResourceId: Int = R.layout.fragment_plan
     override val viewModel: PlanViewModel by viewModel()
+    private var currentPlanType : PlanType = PlanType.PLAN_16_8
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         viewDataBinding.vm = viewModel
 
-        activity?.let {fragmentActivity ->
+        initLiveDataObserver()
+    }
+
+    private fun initLiveDataObserver() {
+        activity?.let { fragmentActivity ->
             viewModel.planType.observe(
                 fragmentActivity,
                 Observer { planType ->
@@ -35,13 +39,24 @@ class PlanFragment : BaseFragment<FragmentPlanBinding, PlanViewModel>() {
         }
     }
 
-
     private fun setPlanTypeView (planType: PlanType) {
-        when (planType) {
+        if (currentPlanType == planType) {
+            return
+        } else {
+            currentPlanType = planType
+        }
 
-            //각 PlanType 별로 시간 or 요일 설정으로 변경
-            PlanType.PLAN_16_8 -> Toast.makeText(activity, "16/8 타입의 다이어트 계획을 설정 합니다.", Toast.LENGTH_LONG).show()
-            else -> Toast.makeText(activity, "5/2 타입의 다이어트 계획을 설정 합니다.", Toast.LENGTH_LONG).show()
+        when (currentPlanType) {
+            PlanType.PLAN_16_8 -> {
+                Toast.makeText(activity, "16/8 다이어트 타입을 설정 합니다.", Toast.LENGTH_LONG).show()
+                plan1Layout.visibility = View.VISIBLE
+                plan2Layout.visibility = View.GONE
+            }
+            else -> {
+                Toast.makeText(activity, "5/2 다이어트 타입을 설정 합니다.", Toast.LENGTH_LONG).show()
+                plan1Layout.visibility = View.GONE
+                plan2Layout.visibility = View.VISIBLE
+            }
         }
     }
 }
--- app/src/main/java/com/goldforest/capdiet/viewmodel/PlanViewModel.kt
+++ app/src/main/java/com/goldforest/capdiet/viewmodel/PlanViewModel.kt
@@ -1,8 +1,6 @@
 package com.goldforest.capdiet.viewmodel
 
-import android.view.View
 import androidx.lifecycle.MutableLiveData
-import com.goldforest.capdiet.R
 import com.goldforest.capdiet.base.BaseViewModel
 import com.goldforest.capdiet.viewmodel.PlanType.*
 
@@ -13,18 +11,11 @@ enum class PlanType {
 
 class PlanViewModel : BaseViewModel() {
     var planType : MutableLiveData<PlanType> = MutableLiveData<PlanType>().apply { postValue(PLAN_16_8) }
-    var isChecked : Int = 0
 
-    val planClickListener = View.OnClickListener {view ->
-        when(view.id) {
-            R.id.rbPlan1 -> {
-                planType.value = PLAN_16_8
-                isChecked = 0
-            } else -> {
-                planType.value = PLAN_5_2
-                isChecked = 1
-            }
+    fun setPlanType (planTypeOrdinal :Int) {
+        planType.value = when (planTypeOrdinal) {
+            PLAN_16_8.ordinal ->  PLAN_16_8
+            else -> PLAN_5_2
         }
     }
-
 }
