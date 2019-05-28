38d553103b91c8def3d62eee9edc8c7a1fccdacc
YeonJung Lim <yeonjung.lim@hp.com>
2019-05-28 13:42:00 +0900

    UI Design - appbar configuration & drawer layout ing..

--- app/src/main/java/com/goldforest/capdiet/view/MainActivity.kt
+++ app/src/main/java/com/goldforest/capdiet/view/MainActivity.kt
@@ -2,12 +2,30 @@ package com.goldforest.capdiet.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.goldforest.capdiet.R
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController by lazy {
+        findNavController(R.id.fragment)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.splashFragment, R.id.mainFragment), drawer_layout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
