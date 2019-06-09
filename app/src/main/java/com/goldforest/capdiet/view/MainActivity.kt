f373a5016a6a57965b968b0a77338c1d7eb58d4b
hyeongju-kim <kinghj.kim@gmail.com>
2019-06-10 01:47:46 +0900

    External Storage Permission 처리

--- app/src/main/java/com/goldforest/capdiet/view/MainActivity.kt
+++ app/src/main/java/com/goldforest/capdiet/view/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.goldforest.capdiet.view
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.onNavDestinationSelected
@@ -17,6 +21,8 @@ import com.goldforest.capdiet.viewmodel.PlanViewModel
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -62,6 +68,26 @@ class MainActivity : AppCompatActivity() {
             drawer_layout.closeDrawers()
         } else {
             super.onBackPressed()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            SplashFragment.REQUEST_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "데이터 저장을 위한 저장소의 권한이 승인됨", Toast.LENGTH_LONG).show()
+                    Handler().postDelayed({
+                        findNavController(R.id.fragment).navigate(R.id.action_splashFragment_to_mainFragment)
+                    }, 2000)
+                } else {
+                    Toast.makeText(this, "데이터 저장을 위한 저장소의 권한이 거부됨", Toast.LENGTH_LONG).show()
+                }
+            }
+
+
         }
     }
 }
