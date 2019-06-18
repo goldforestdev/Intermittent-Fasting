aaa1e6f8151897e30f2486ab7231c6c4549ec5da
YeonJung Lim <yeonjung.lim@hp.com>
2019-06-18 16:24:41 +0900

    plan list adapter 연결

--- app/src/main/java/com/goldforest/capdiet/view/planlist/PlanListFragment.kt
+++ app/src/main/java/com/goldforest/capdiet/view/planlist/PlanListFragment.kt
@@ -2,11 +2,16 @@ package com.goldforest.capdiet.view.planlist
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import com.goldforest.capdiet.R
 import com.goldforest.capdiet.base.BaseFragment
 import com.goldforest.capdiet.databinding.FragmentPlanListBinding
 import com.goldforest.capdiet.viewmodel.PlanListViewModel
+import com.goldforest.domain.model.Plan
+import kotlinx.android.synthetic.main.fragment_plan_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -14,6 +19,7 @@ class PlanListFragment : BaseFragment<FragmentPlanListBinding, PlanListViewModel
 
     override val layoutResourceId: Int = R.layout.fragment_plan_list
     override val viewModel: PlanListViewModel by viewModel()
+    private val adapter = PlanListAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -23,6 +29,21 @@ class PlanListFragment : BaseFragment<FragmentPlanListBinding, PlanListViewModel
     }
 
     private fun initAdapter() {
+        rv_plan_list.adapter = adapter
+        viewModel.planList.observe(this, Observer<PagedList<Plan>> {
+            Log.d("Activity", "list: ${it?.size}")
+            showEmptyList(it?.size == 0)
+            adapter.submitList(it)
+        })
+    }
 
+    private fun showEmptyList(show: Boolean) {
+        if (show) {
+            emptyList.visibility = View.VISIBLE
+            rv_plan_list.visibility = View.GONE
+        } else {
+            emptyList.visibility = View.GONE
+            rv_plan_list.visibility = View.VISIBLE
+        }
     }
 }
